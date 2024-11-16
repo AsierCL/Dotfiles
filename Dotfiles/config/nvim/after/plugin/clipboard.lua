8f3e54c0a675e681834748ae564093d4a94944ab
AsierCL <asiercl.05@gmail.com>
2024-11-16 17:49:25 +0100

    Añado carpetas

---
+++ Dotfiles/config/nvim/after/plugin/clipboard.lua
@@ -0,0 +1,13 @@
+vim.g.clipboard = {
+    name = 'xclip',
+    copy = {
+        ['+'] = 'xclip',
+        ['*'] = 'xclip',
+    },
+    paste = {
+        ['+'] = 'xclip',
+        ['*'] = 'xclip',
+    },
+    cache_enabled = 0,
+}
+
---
+++ Dotfiles/config/nvim/lua/hugo/init.lua
@@ -0,0 +1,43 @@
+require("hugo.set")
+require("hugo.remap")
+require("hugo.packer")
+
+
+local augroup = vim.api.nvim_create_augroup
+local ThePrimeagenGroup = augroup('ThePrimeagen', {})
+
+local autocmd = vim.api.nvim_create_autocmd
+local yank_group = augroup('HighlightYank', {})
+
+function R(name)
+    require("plenary.reload").reload_module(name)
+end
+
+autocmd('TextYankPost', {
+    group = yank_group,
+    pattern = '*',
+    callback = function()
+        vim.highlight.on_yank({
+            higroup = 'IncSearch',
+            timeout = 40,
+        })
+    end
+})
+
+autocmd({ "BufWritePre" }, {
+    group = ThePrimeagenGroup,
+    pattern = "*",
+    command = [[%s/\s\+$//e]],
+})
+
+autocmd({ "BufWinEnter" }, {
+    pattern = { '*.md' },
+    callback = function()
+        vim.opt.wrap = true
+        vim.opt.linebreak = true
+    end,
+})
+
+vim.g.netrw_browse_split = 0
+vim.g.netrw_banner = 0
+vim.g.netrw_winsize = 25
